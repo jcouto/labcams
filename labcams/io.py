80f3987f25fb5a7a1b4f0ba27503a5ab75c2f79f
Ben Vermaercke <ben.vermaercke@gmail.com>
2017-09-27 19:12:14 +0200

    Triggered mode works.

--- labcams/io.py
+++ labcams/io.py
@@ -4,6 +4,7 @@
 import time
 import sys
 from multiprocessing import Process,Queue,Event,Array,Value
+from ctypes import c_long, c_char_p
 from datetime import datetime
 import time
 import sys
@@ -11,14 +12,14 @@ from .utils import display
 import numpy as np
 import os
 from os.path import join as pjoin
-from ctypes import c_long
 from tifffile import TiffWriter as twriter
 
 class TiffWriter(Process):
     def __init__(self,inQ = None, loggerQ = None,
-                 expName = 'dummy',dataName = 'eyecam',
+                 filename = 'dummy\\run',
+                 dataName = 'eyecam',
                  dataFolder=pjoin(os.path.expanduser('~'),'data'),
-                 framesPerFile=500,
+                 framesPerFile=256,
                  sleepTime = 1./30,
                  incrementRuns=True,
                  compression=None):
@@ -29,7 +30,7 @@ class TiffWriter(Process):
         self.close = Event()
         self.sleepTime = sleepTime # seconds
         self.framesPerFile = framesPerFile
-        self.expName = expName
+        self.filename = Array('u',' ' * 1024)
         self.dataFolder = dataFolder
         self.dataName = dataName
         self.folderName = None
@@ -41,9 +42,21 @@ class TiffWriter(Process):
         self.inQ = inQ
         self.today = datetime.today().strftime('%Y%m%d')
 
+    def setFilename(self,filename):
+        self.write.clear()
+        for i in range(len(self.filename)):
+            self.filename[i] = ' '
+        for i in range(len(filename)):
+            self.filename[i] = filename[i]
+        display('Filename updated: ' + self.getFilename())
+    
+    def getFilename(self):
+        return str(self.filename[:]).strip(' ')
+        
     def stop(self):
         self.write.clear()
         self.close.set()
+        self.join()
 
     def closeFile(self):
         if not self.fd is None:
@@ -52,12 +65,11 @@ class TiffWriter(Process):
 
     def openFile(self,nfiles = None):
         nfiles = self.nFiles
-        folder = pjoin(self.dataFolder,self.dataName,self.expName)
+        folder = pjoin(self.dataFolder,self.dataName,self.getFilename())
         if not os.path.exists(folder):
             os.makedirs(folder)
-        filename = pjoin(folder,'{0}_{1}_run{2:03d}_{3:08d}.tif'.format(
+        filename = pjoin(folder,'{0}_run{1:03d}_{2:08d}.tif'.format(
             self.today,
-            self.expName,
             self.runs.value,
             nfiles))
         if not self.fd is None:
@@ -69,38 +81,35 @@ class TiffWriter(Process):
         while not self.close.is_set():
             self.frameCount.value = 0
             self.nFiles = 0
-#            if self.incrementRuns:
-#                self.runs.value += 1
-            
             while self.write.is_set():
-                if self.fd is None:
-                    self.openFile(self.nFiles) 
-                while not self.inQ.empty():
+                if not self.inQ.empty():
                     buff = self.inQ.get()
                     try:
                         frame,(frameid,timestamp,) = buff
                     except:
-                        print(buff)
+                        display(buff)
                         continue
-                    self.frameCount.value += 1
                     if np.mod(self.frameCount.value,self.framesPerFile)==0:
                         self.openFile()
                         display('Queue size: {0}'.format(self.inQ.qsize()))
-                    self.fd.save(frame,compress=self.compression,
-                                 description='id:{0};timestamp:{1}'.format(frameid,timestamp))
+                    self.fd.save(frame,
+                                 compress=self.compression,
+                                 description='id:{0};timestamp:{1}'.format(frameid,
+                                                                           timestamp))
+                    self.frameCount.value += 1
             # If queue is not empty, empty if to files.
             while not self.inQ.empty():
                 buff = self.inQ.get()
                 try:
                     frame,(frameid,timestamp,) = buff
                 except:
-                    print(buff)
+                    display(buff)
                     continue
-                self.frameCount.value += 1
                 if np.mod(self.frameCount.value,self.framesPerFile)==0:
                     self.openFile()
                 self.fd.save(frame,compress=self.compression,
                              description='id:{0};timestamp:{1}'.format(frameid,timestamp))
+                self.frameCount.value += 1
 
             self.closeFile()
             if not self.frameCount.value == 0:
@@ -108,10 +117,10 @@ class TiffWriter(Process):
                     self.frameCount.value,
                     self.dataName,
                     self.nFiles))
+                
             #self.closeFile()
             # spare the processor just in case...
             time.sleep(self.sleepTime)
 
             
-# Add 2 photosensors, one for the queues and one for the lap; queue positions are logged as well and all is good. This is the easiest way to get both as long as the belt does is always at the same position, to do that put the encoder just after the animal with holes so the belt does not slip.
         
