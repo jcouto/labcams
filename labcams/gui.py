db6d822d4ff427cbb92be317a94d0132b80a9ffb
Joao Couto <jpcouto@gmail.com>
2018-05-04 15:35:06 +0200

    Subtract background. No save option.

--- labcams/gui.py
+++ labcams/gui.py
@@ -102,6 +102,8 @@ class LabCamsGUI(QMainWindow):
         connected_avt_cams = []
         for c,cam in enumerate(self.cam_descriptions):
             display("Connecting to camera [" + str(c) + '] : '+cam['name'])
+            if not 'Save' in cam.keys():
+                cam['Save'] = True
             if cam['driver'] == 'AVT':
                 camids = [camid for (camid,name) in zip(avtids,avtnames) 
                           if cam['name'] in name]
@@ -114,14 +116,15 @@ class LabCamsGUI(QMainWindow):
                 if not 'TriggerMode' in cam.keys():
                     cam['TriggerMode'] = 'LevelHigh'
                 self.camQueues.append(Queue())
-                
-                self.writers.append(TiffWriter(inQ = self.camQueues[-1],
-                                               dataFolder=self.parameters['recorder_path'],
-                                               framesPerFile=self.parameters['recorder_frames_per_file'],
-                                               sleepTime = self.parameters['recorder_sleep_time'],
-                                               filename = expName,
-                                               dataName = cam['description']))
-                
+                if cam['Save']:
+                    self.writers.append(TiffWriter(inQ = self.camQueues[-1],
+                                                   dataFolder=self.parameters['recorder_path'],
+                                                   framesPerFile=self.parameters['recorder_frames_per_file'],
+                                                   sleepTime = self.parameters['recorder_sleep_time'],
+                                                   filename = expName,
+                                                   dataName = cam['description']))
+                else:
+                    self.writers.append(None)
                 self.cams.append(AVTCam(camId=camids[0],
                                         outQ = self.camQueues[-1],
                                         frameRate=cam['frameRate'],
@@ -133,9 +136,12 @@ class LabCamsGUI(QMainWindow):
             elif cam['driver'] == 'QImaging':
             	display('Connecting to Qimaging camera.')
                 self.camQueues.append(Queue())
-                self.writers.append(TiffWriter(inQ = self.camQueues[-1],
-                                                  filename = expName,
-                                                  dataName = cam['description']))
+                if cam['Save']:
+                    self.writers.append(TiffWriter(inQ = self.camQueues[-1],
+                                                   filename = expName,
+                                                   dataName = cam['description']))
+                else:
+                    self.writers.append(None)
                 if not 'binning' in cam.keys():
                     cam['binning'] = 2
                 self.cams.append(QImagingCam(camId=cam['id'],
@@ -147,7 +153,8 @@ class LabCamsGUI(QMainWindow):
                                              triggered = self.triggered))
             else:
             	display('Unknown camera driver' + cam['driver'])
-            self.writers[-1].daemon = True
+            if cam['Save']:
+                self.writers[-1].daemon = True
             self.cams[-1].daemon = True
 #        self.resize(500,700)
 
@@ -161,18 +168,20 @@ class LabCamsGUI(QMainWindow):
         self.camerasRunning = False
         for cam,writer in zip(self.cams,self.writers):
             cam.start()
-            writer.start()
+            if not writer is None:
+                writer.start()
         camready = 0
         while camready != len(self.cams):
             camready = np.sum([cam.cameraReady.is_set() for cam in self.cams])
         display('Initialized cameras.')
         self.zmqTimer = QTimer()
         self.zmqTimer.timeout.connect(self.zmqActions)
-        self.zmqTimer.start(1000)
+        self.zmqTimer.start(500)
         self.triggerCams(save=self.saveOnStart)
     def setExperimentName(self,expname):
         for writer in self.writers:
-            writer.setFilename(expname)
+            if not writer is None:
+                writer.setFilename(expname)
         time.sleep(1)
         self.recController.experimentNameEdit.setText(expname)
         
@@ -193,12 +202,14 @@ class LabCamsGUI(QMainWindow):
     def triggerCams(self,save=False):
         if save:
             for c,(cam,writer) in enumerate(zip(self.cams,self.writers)):
-                cam.saving.set()
-                writer.write.set()
+                if not writer is None:
+                    cam.saving.set()
+                    writer.write.set()
         else:
             for c,(cam,writer) in enumerate(zip(self.cams,self.writers)):
-                cam.saving.clear()
-                writer.write.clear()
+                if not writer is None:
+                    cam.saving.clear()
+                    writer.write.clear()
         for c,cam in enumerate(self.cams):
             cam.startTrigger.set()
         display('Triggered cameras.')
@@ -218,19 +229,9 @@ class LabCamsGUI(QMainWindow):
         for c,cam in enumerate(self.cams):
             self.tabs.append(QDockWidget("Camera: "+str(c),self))
             layout = QVBoxLayout()
-            if 'trackEye' in self.cam_descriptions[c].keys():
-                trackeye = True
-            else:
-                trackeye = None
-            if 'subtractBackground' in self.cam_descriptions[c].keys():
-                subtractBackground = True
-            else:
-                subtractBackground = False
-            
             self.camwidgets.append(CamWidget(frame = np.zeros((cam.h,cam.w),
                                                               dtype=cam.dtype),
-                                             trackeye=trackeye,
-                                             subtractBackground = subtractBackground))
+                                             parameters = self.cam_descriptions[c]))
             self.tabs[-1].setWidget(self.camwidgets[-1])
             self.tabs[-1].setFloating(False)
             if c < 2:
@@ -276,20 +277,22 @@ class LabCamsGUI(QMainWindow):
             cam.stop_acquisition()
         display('Acquisition duration:')
         for c,(cam,writer) in enumerate(zip(self.cams,self.writers)):
-            cam.saving.clear()
-            writer.write.clear()
+            if not writer is None:
+                cam.saving.clear()
+                writer.write.clear()
+                writer.stop()
             cam.stop()
-            writer.stop()
         for c in self.cams:
             c.join()
         for c,(cam,writer) in enumerate(zip(self.cams,self.writers)):
-            display('   ' + self.cam_descriptions[c]['name']+
-                  ' [ Acquired:'+
-                  str(cam.nframes.value) + ' - Saved: ' + 
-                  str(writer.frameCount.value) + ' - Frames/rate: '
-                  +str(cam.nframes.value/
-                       self.cam_descriptions[c]['frameRate']) +']')
-
+            if not writer is None:
+                display('   ' + self.cam_descriptions[c]['name']+
+                        ' [ Acquired:'+
+                        str(cam.nframes.value) + ' - Saved: ' + 
+                        str(writer.frameCount.value) + ' - Frames/rate: '
+                        +str(cam.nframes.value/
+                             self.cam_descriptions[c]['frameRate']) +']')
+                writer.join()
         event.accept()
 
 class RecordingControlWidget(QWidget):
@@ -363,39 +366,46 @@ class RecordingControlWidget(QWidget):
         self.parent.triggerCams(save = state)
         
 class CamWidget(QWidget):
-    def __init__(self,frame, trackeye=None,subtractBackground = False):
+    def __init__(self,frame, parameters = None):
         super(CamWidget,self).__init__()
         self.scene=QGraphicsScene(0,0,frame.shape[1],
                                   frame.shape[0],self)
         self.view = QGraphicsView(self.scene, self)
         self.lastFrame = None
         self.lastnFrame = 0
-        if not trackeye is None:
-            trackeye = MPTracker(drawProcessedFrame=True)
-        self.eyeTracker = trackeye
-        self.trackEye = True
-        if subtractBackground:
+        if not 'SubtractBackground' in parameters.keys():
+            parameters['SubtractBackground'] = False
+        if not 'TrackEye' in parameters.keys():
+            parameters['TrackEye'] = False
+        self.parameters = parameters
+        self.eyeTracker = None
+        if self.parameters['TrackEye']:
+            self.eyeTracker = MPTracker(drawProcessedFrame=True)
+        if self.parameters['SubtractBackground']:
             self.lastFrame = frame.copy()
+        self.string = '{0}'
+        if not self.parameters['Save']:
+            self.string = 'no save -{0}'
         self.image(np.array(frame),-1)
         self.show()
     def image(self,image,nframe):
         if self.lastnFrame != nframe:
             self.scene.clear()
-            if not self.eyeTracker is None and self.trackEye:
+            if not self.eyeTracker is None:
                 img = self.eyeTracker.apply(image.copy()) 
                 frame = self.eyeTracker.img
             else:
                 if not self.lastFrame is None:
-                    frame = 2*(image.copy().astype(np.int16) -
-                               self.lastFrame.astype(np.int16)) + 128
-                    self.lastFrame = (1-1/10.)*(self.lastFrame.astype(np.float32)) + (1/10.)*image.copy().astype(np.float32)
+                    frame = image.copy() - self.lastFrame
+                    nacum = 3.
+                    self.lastFrame = (1-1/nacum)*(self.lastFrame.astype(np.float32)) + (1/nacum)*image.copy().astype(np.float32)
                 else:
                     frame = image
-            if frame.dtype == np.uint16:
+            if self.parameters['driver'] == 'QImaging':
                 frame = np.array((frame.astype(np.float32)/2.**14)*2.**8).astype(np.uint8)
             if len(frame.shape) == 2:
                 frame = cv2.cvtColor(frame.astype(np.uint8), cv2.COLOR_GRAY2BGR)
-            cv2.putText(frame,str(nframe), (10,100), cv2.FONT_HERSHEY_SIMPLEX,
+            cv2.putText(frame,self.string.format(nframe), (10,100), cv2.FONT_HERSHEY_SIMPLEX,
                         1, 105,2)
             self.qimage = QImage(frame, frame.shape[1], frame.shape[0], 
                                  frame.strides[0], QImage.Format_RGB888)
@@ -413,15 +423,18 @@ DEFAULTS = dict(cams = [{'description':'facecam',
                          'gain':10,
                          'frameRate':150.,
                          'TriggerSource':'Line1',
-                         'TriggerMode':'LevelHigh'},
+                         'TriggerMode':'LevelHigh',
+                         'SubtractBackground':True,
+                         'Save':True},
                         {'description':'eyecam',
                          'name':'GC660M',
                          'driver':'AVT',
                          'gain':10,
-                         'trackEye':True,
+                         'TrackEye':True,
                          'frameRate':31.,
                          'TriggerSource':'Line1',
-                         'TriggerMode':'LevelHigh'},
+                         'TriggerMode':'LevelHigh',
+                         'Save':False},
                         {'description':'1photon',
                          'name':'qcam',
                          'id':0,
