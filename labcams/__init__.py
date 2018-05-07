88a0085dd74f2266c0badb3c3af411e7436eb57f
Joao Couto <jpcouto@gmail.com>
2018-05-07 14:25:45 +0200

    Stimulus averages.

--- labcams/__init__.py
+++ labcams/__init__.py
@@ -1,4 +1,8 @@
 from .cams import AVT_get_ids, AVTCam,QImagingCam
 from .io import TiffWriter,parseCamLog,TiffStack
-from .utils import display, getPreferences,cameraTimesFromVStimLog,findVStimLog
+from .utils import (display, getPreferences,
+                    cameraTimesFromVStimLog,
+                    findVStimLog,
+                    triggeredAverage,
+                    triggeredTrials)
 
