8dd3000a91ad31fcf7ff3185a3eee91616101f95
Joao Couto <jpcouto@gmail.com>
2019-10-25 10:58:59 +0200

    Ximea triggered on input1 and out1 sends pulse when frame acquired (computer controlled quick fix).

--- labcams/ximeacam.py
+++ labcams/ximeacam.py
@@ -8,14 +8,17 @@ class XimeaCam(GenericCam):
                  binning = 2,
                  exposure = 20000,
                  triggerSource = np.uint16(2),
+                 outputs = ['XI_GPO_EXPOSURE_ACTIVE'],
                  triggered = Event(),
                  **kwargs):
         super(XimeaCam,self).__init__()
         self.drivername = 'ximea'
         if camId is None:
                 display('[Ximea] - Need to supply a camera ID.')
+        self.triggered = triggered
         self.camId = camId
         self.queue = outQ
+        self.outputs = outputs
         self.binning = binning
         self.exposure = exposure
         frame = self.get_one()
@@ -25,9 +28,7 @@ class XimeaCam(GenericCam):
         self.nchannels = 1
         self.dtype = np.uint16
         self._init_variables(self.dtype)
-        self.triggered = triggered
         self.triggerSource = triggerSource
-
         self.img[:] = np.reshape(frame,self.img.shape)[:]
 
         display("[Ximea {0}] - got info from camera.".format(self.camId))
@@ -39,9 +40,9 @@ class XimeaCam(GenericCam):
                 widget = 'float',
                 variable = 'exposure',
                 units = 'ms',
-                type = 'float',
-                min = 0.001,
-                max = 100000,
+                type = 'int',
+                min = 1,
+                max = 10000000000,
                 step = 10))
     
     def get_one(self):
@@ -63,33 +64,60 @@ class XimeaCam(GenericCam):
             display('[Ximea {0}] Setting exposure to {1} ms.'.format(
                 self.cam_id, self.exposure/1000.))
 
-    def _cam_init(self):
+    def _cam_init(self,set_gpio=True):
         self.cam = xiapi.Camera()
         #start communication
         self.cam.open_device()
+        self.cam.set_acq_timing_mode('XI_ACQ_TIMING_MODE_FREE_RUN')
         self.cam.set_exposure(self.exposure)
         self.cam.set_binning_vertical(self.binning)
         self.cam.set_binning_horizontal(self.binning)
+        # Set the GPIO
+        self.cam.set_gpi_selector('XI_GPI_PORT1')
+        self.cam.set_trigger_source('XI_TRG_OFF')
+
+        if set_gpio:
+            self.cam.set_gpo_selector('XI_GPO_PORT1')
+            # for now set the GPO to blink in software (port1 will do that for sync purposes, the test cam does not support XI_GPO_EXPOSURE_PULSE)
+            self.cam.set_gpo_mode('XI_GPO_ON'); #XI_GPO_EXPOSURE_PULSE
+            if self.triggered.is_set():
+                self.cam.set_gpi_mode('XI_GPI_TRIGGER')
+                self.cam.set_trigger_source('XI_TRG_EDGE_RISING')
+        print()
+        self.cam.set_led_selector('XI_LED_SEL1')
+        self.cam.set_led_mode('XI_LED_OFF')
+        self.cam.set_led_selector('XI_LED_SEL2')
+        self.cam.set_led_mode('XI_LED_FRAME_ACTIVE')
+        self.cam.set_led_selector('XI_LED_SEL3')
+        self.cam.set_led_mode('XI_LED_EXPOSURE_ACTIVE')
         self.cambuf = xiapi.Image()
         self.camera_ready.set()
+        self.lastframeid = -1
         self.nframes.value = 0
 
     def _cam_startacquisition(self):
         self.cam.start_acquisition()
 
     def _cam_loop(self):
-        self.cam.get_image(self.cambuf)
+        try:
+            self.cam.get_image(self.cambuf)
+        except xiapi.Xi_error:
+            return
         frame = self.cambuf.get_image_data_numpy()
-        frameID = self.nframes.value
-        self.nframes.value += 1
+        frameID = self.cambuf.nframe
         timestamp = self.cambuf.tsUSec
         if self.saving.is_set():
             if not frameID == self.lastframeid :
                 self.queue.put((frame.copy(),
                                 (frameID,timestamp)))
+        if not frameID == self.lastframeid:
+            self.buf[:] = np.reshape(frame.copy(),self.buf.shape)[:]
+            self.cam.set_gpo_mode('XI_GPO_OFF'); #XI_GPO_EXPOSURE_PULSE
+            time.sleep(0.001)
+            self.cam.set_gpo_mode('XI_GPO_ON'); #XI_GPO_EXPOSURE_PULSE
+            self.nframes.value += 1
         self.lastframeid = frameID
-        self.buf[:] = np.reshape(frame.copy(),self.buf.shape)[:]
-
+        
     def _cam_close(self):
         self.cam.stop_acquisition()
         self.cam.close_device()
