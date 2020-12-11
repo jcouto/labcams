ebff7013e65c5b68f3e23e816a8f0cbf2e4c1ede
Joao Couto <jpcouto@gmail.com>
2020-12-10 22:53:57 -0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,36 @@
-
 #!/usr/bin/env python
 # Install script for labcams.
 # Joao Couto - March 2017
-
 import os
 from os.path import join as pjoin
 from setuptools import setup
 from setuptools.command.install import install
 
+with open("README.md", "r") as fh:
+    longdescription = fh.read()
 
-longdescription = ''' Recorder for behavioral cameras and one-photon imaging.'''
+requirements = []
+with open("requirements.txt","r") as f:
+    requirements = f.read().splitlines()
+    
 data_path = pjoin(os.path.expanduser('~'), 'labcams')
 
 setup(
-  name = 'labcams',
-  version = '0.1b',
-  author = 'Joao Couto',
-  author_email = 'jpcouto@gmail.com',
-  description = (longdescription),
-  long_description = longdescription,
-  license = 'GPL',
-  packages = ['labcams'],
-  entry_points = {
+    name = 'labcams',
+    version = '0.2',
+    author = 'Joao Couto',
+    author_email = 'jpcouto@gmail.com',
+    description = (longdescription),
+    long_description = longdescription,
+    license = 'GPL',
+    install_requires = requirements,
+    url = "https://bitbucket.org/jpcouto/labcams",
+    packages = ['labcams'],
+    entry_points = {
         'console_scripts': [
-          'labcams = labcams.gui:main',
+            'labcams = labcams.gui:main',
         ]
-        },
-#    data_files=[(data_path, [pjoin('configurations',
-#                                   'spyking-circus-invivo.params')]),
-#    ],
-
-    )
+    },
+)
 
 
