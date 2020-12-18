7d85063d2055fefb4463813d0de02793124cbee1
Joao Couto <jpcouto@gmail.com>
2020-12-17 20:09:13 -0800

    Write to recorder log on noqueue.

--- labcams/cams.py
+++ labcams/cams.py
@@ -209,7 +209,7 @@ class GenericCam(Process):
                     if self.recorder is None:
                         self.queue.put([msg])
                     else:
-                        self.logfile.write(msg)
+                        self.recorder.logfile.write(msg)
                     
     def _call_event(self,eventname,eventvalue):
         if eventname in self.ctrevents.keys():
